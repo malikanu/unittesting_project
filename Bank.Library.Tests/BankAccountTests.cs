ab38a7098a8c6b58eda551cc166fdaabe9c286c3
anu <anu.malik@cygrp.com>
2018-01-22 17:28:52 +0530

    initial commit

---
+++ Bank.Library.Tests/BankAccountTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bank.Library.Tests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void Credit_ValidAmount_UpdateBalance()
+        {
+            // To create a test methoid follow -> AAA's Pattern
+
+            //Arrange
+            double currentBalance = 11.99;
+            double creditAmount = 4.55;
+            double expected = 16.54;
+            BankAccount account = new BankAccount("Mr. Bryan Adams", currentBalance);
+
+            //Act
+            account.Credit(creditAmount);
+
+            //Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not credited correctly");
+           
+
+        }
+
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+
+            //Arrange
+            double currentBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Adams", currentBalance);
+
+            //Act
+            account.Debit(debitAmount);
+
+            //Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+
+        }
+
+        // To Test: The method throws an ArgumentOutOfRangeException if the debit amount is less than zero.
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            //Arrange
+            double currentBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Adams", currentBalance);
+
+            //Act
+            account.Debit(debitAmount);
+
+            //Assert is handled by ExpectedException
+        }
+
+        // To Test: The method throws an ArgumentOutOfRangeException if the debit amount exceeds the balance.
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            //Arrange
+            double currentBalance = 11.99;
+            double debitAmount = 100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Adams", currentBalance);
+
+            //Act
+            account.Debit(debitAmount);
+
+            //Assert is handled by ExpectedException
+        }
+
+    }
+}
---
+++ Bank/BankAccount.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Library
+{
+    // Bank account Demo Class
+    public class BankAccount
+    {
+        private string m_customerName;
+        private double m_balance;
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public String CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        // Method to withdraw amount
+        public void Debit(double amount)
+        {
+
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("Debit amount exceeds balance");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("Debit amount less than zero");
+            }
+
+
+            m_balance -= amount; // Intentionaly incorrect code
+        }
+
+        // Method to deposit amount
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("Debit amount less than zero");
+            }
+
+            m_balance += amount;
+        }
+
+
+        /* By analyzing the methods under test (Debit & Credit), we determine that there are at least three behaviors that need to be checked:
+             1. The method throws an ArgumentOutOfRangeException if the debit amount is greater than the balance.
+             2. It also throws ArgumentOutOfRangeException if the debit amount is less than zero.
+             3. If the checks in 1.) and 2.) are satisfied, the method adds and deducts the amount from the account balance.*/
+    }
+}
